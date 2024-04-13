5cbcc83ca116059c75f099427d5b53d548c96d1c
爱吃橘子 <103941663+IMarx666@users.noreply.github.com>
2024-04-13 20:43:16 +0800

    feat：增加对话重置接口

--- src/main/java/com/example/pet_hospital/Controller/AIController.java
+++ src/main/java/com/example/pet_hospital/Controller/AIController.java
@@ -5,7 +5,9 @@ import com.example.pet_hospital.Entity.query;
 import com.example.pet_hospital.Entity.result;
 import com.example.pet_hospital.Service.AIService;
 import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -16,17 +18,14 @@ public class AIController {
 
 
     @Autowired
-    private  AIService aiService;
-
-
-
+    private AIService aiService;
 
 
     @PostMapping("/ai")
     public result getErnieBotTurboResponse(@RequestBody query query,
                                            HttpSession session,
                                            @RequestHeader("Authorization") String Authorization
-                                            ) {
+    ) {
 
         //不能为空
         if (query.getQuery() == null || query.getQuery().isEmpty()) {
@@ -41,6 +40,17 @@ public class AIController {
             return result.error("Error while processing the request: " + e.getMessage());
         }
 
+    }
+
+    //重置ai对话
+    @PostMapping("/reset")
+    public result reset(HttpServletRequest request, @RequestHeader("Authorization") String Authorization) {
+        HttpSession session = request.getSession(false);  // 获取当前会话，如果不存在则返回null
+        if (session != null) {
+            session.invalidate();  // 使当前会话失效
+        }
+        session = request.getSession(true);
+        return result.success("会话已重置");
     }
 
 }
